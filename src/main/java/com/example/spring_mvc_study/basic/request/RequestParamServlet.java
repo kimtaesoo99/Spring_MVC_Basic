acfec1b1e8821449ae2b5ae1e0961b87e2ceac7d
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-20 20:49:44 +0900

    feat: 서블렛 쿼리 파라미터 request 확인 메서드 추가

---
+++ src/main/java/com/example/spring_mvc_study/basic/request/RequestParamServlet.java
@@ -0,0 +1,38 @@
+package com.example.spring_mvc_study.basic.request;
+
+import com.example.spring_mvc_study.basic.HelloServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestParamServlet",urlPatterns = "/request-param")
+public class RequestParamServlet extends HelloServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("[전체 파라미터 조회] - start");
+
+        request.getParameterNames().asIterator()
+            .forEachRemaining(paramName -> System.out.println(paramName +"=" +request.getParameter(paramName)));
+
+        System.out.println("[전체 파라미터 조회] - end");
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames = request.getParameterValues("username");
+        for (String s : usernames) {
+            System.out.println("username = "+ s);
+        }
+
+        response.getWriter().write("ok");
+
+    }
+}
