f7f796c174da80c99df3b5656036d00c560c0e7c
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-20 21:20:10 +0900

    feat: 테스트용 객체 추가

---
+++ src/main/java/com/example/spring_mvc_study/basic/HelloData.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_study.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
