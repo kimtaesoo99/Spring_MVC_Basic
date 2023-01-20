ea688bebb70f9dbc008eea40753d38944d51adf7
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-20 18:52:34 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_mvc_study/SpringMvcStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_mvc_study/SpringMvcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
