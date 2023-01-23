14425b58522c0b0921fd7c9b2cef04e08cfe0f62
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-23 19:41:18 +0900

    feat: 프론트 컨트롤러 서블릿 v4 를 사용한 멤버가입폼 리턴

---
+++ src/main/java/com/example/spring_mvc_study/web/frontcontroller/v4/controller/MemberFormControllerV4.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_study.web.frontcontroller.v4.controller;
+
+import com.example.spring_mvc_study.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberFormControllerV4 implements ControllerV4 {
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        return "new-form";
+    }
+}
