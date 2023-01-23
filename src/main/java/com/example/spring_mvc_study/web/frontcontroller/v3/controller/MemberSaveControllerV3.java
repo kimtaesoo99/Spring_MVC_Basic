18faedeea44da45a2bd09fe56855e08827c2ae56
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-23 18:21:42 +0900

    feat: 멤버 저장이 폼 v3 추가

---
+++ src/main/java/com/example/spring_mvc_study/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,27 @@
+package com.example.spring_mvc_study.web.frontcontroller.v3.controller;
+
+import com.example.spring_mvc_study.domain.member.Member;
+import com.example.spring_mvc_study.domain.member.MemberRepository;
+import com.example.spring_mvc_study.web.frontcontroller.ModelView;
+import com.example.spring_mvc_study.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelView mv = new ModelView("save-result");
+        mv.getModel().put("member", member);
+        return mv;
+
+    }
+}
